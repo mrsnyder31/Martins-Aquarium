14fc3e2be7df6ebe30dd51df313268f2934bb5b2
Marek Snyder <mr.snyder31@gmail.com>
2022-11-21 11:13:09 -0500

    Final version for self assessment. maybe extra coffee files?

---
+++ scripts/main.js
@@ -0,0 +1,53 @@
+ import { getFish } from './database.js'
+
+const allFish = getFish()
+
+for (const fish of allFish) {
+    console.log(fish)
+}
+ 
+// Import the function that returns a copy of the fish array
+//import { getFish } from './database.js'
+
+export const FishList = () => {
+    // Invoke the function that you imported from the database module
+    const fishes = getFish()
+
+    // Start building a string filled with HTML syntax
+    let htmlString = '<article class="fish-list">'
+
+    // Create HTNL representations of each fish here
+    for (const fish of fishes) {
+
+        // Why is there a backtick used for this string?
+        htmlString += `<section class="fish-card">
+            <div><img  class="fish__image image--card" src="${fish.fishImage}" /></div>
+            <div class="fish__name">${fish.fishName}</div>
+            <div class="fish__species">${fish.fishSpecies}</div>
+            <div class="fish__length">${fish.fishLength}</div>
+            <div class="fish__location">${fish.harvestLocation}</div>
+            <div class="fish__diet">${fish.fishDiet}</div>
+        </section>
+`
+    }
+    htmlString += `</article>`
+
+    return htmlString
+}
+
+// Import the FishList function from the correct module
+//import { getFish } from './database.js'
+
+/*
+    What is the CSS selector for the element where you
+    want to display the fish?
+
+    Use . for elements with a "class" attribute
+    Use # for elements with an "id" attribute
+ */
+let parentHTMLElement = document.querySelector(".fish-list")
+
+parentHTMLElement.innerHTML = FishList();
+
+//console.log(FishList())
+
